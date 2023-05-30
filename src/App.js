8eaebaad7588f644deb94a42b59d208f77c9c4dd
Lukas Baltrimas <lukasbaltrimas123@gmail.com>
2023-05-30 22:58:51 +0000

    Expenses added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: "$300", date: new Date(2023, 1, 23) },
+    { title: "Toilet paper", amount: "$3", date: new Date(2021, 2, 28) },
+    { title: "Car rapair", amount: "$689", date: new Date(2021, 3, 2) },
+    { title: "School", amount: "$240", date: new Date(2022, 6, 15) },
+    { title: "Birthday", amount: "$65", date: new Date(2022, 7, 12) },
+    { title: "Lunch", amount: "$4", date: new Date(2020, 9, 8) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
