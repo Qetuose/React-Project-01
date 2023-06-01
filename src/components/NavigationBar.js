6510247e760bcc275c12ad8d075d568b2626d540
Lukas Baltrimas <lukasbaltrimas123@gmail.com>
2023-06-01 16:09:36 +0000

    Navigation bar added

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,14 @@
+import "./NavigationBar.css";
+
+function NavigationBar() {
+  return (
+    <div className="navigation-bar">
+      <button className="navigation-bar__button">First</button>
+      <button className="navigation-bar__button">Second</button>
+      <button className="navigation-bar__button">Third</button>
+      <button className="navigation-bar__button">Fourth</button>
+    </div>
+  );
+}
+
+export default NavigationBar;
