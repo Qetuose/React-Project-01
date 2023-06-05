55d5d9b5d323f56525fe05c40e0a61b63ce05e1e
Lukas Baltrimas <lukasbaltrimas123@gmail.com>
2023-06-05 00:11:57 +0000

    NewExpense form update

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import "./components/NavigationBar/NavigationBar.css";
 
 const App = () => {
   const expenses = [
-    { title: "Car Insurance", amount: "$300", date: new Date(2023, 1, 23) },
-    { title: "Toilet paper", amount: "$3", date: new Date(2021, 2, 28) },
-    { title: "Car rapair", amount: "$689", date: new Date(2021, 3, 2) },
-    { title: "School", amount: "$240", date: new Date(2022, 6, 15) },
-    { title: "Birthday", amount: "$65", date: new Date(2022, 7, 12) },
-    { title: "Lunch", amount: "$4", date: new Date(2020, 9, 8) },
+    { id: "e1", title: "Car Insurance", amount: "$300", date: new Date(2023, 1, 23) },
+    { id: "e2", title: "Toilet paper", amount: "$3", date: new Date(2021, 2, 28) },
+    { id: "e3", title: "Car rapair", amount: "$689", date: new Date(2021, 3, 2) },
+    { id: "e4", title: "School", amount: "$240", date: new Date(2022, 6, 15) },
+    { id: "e5", title: "Birthday", amount: "$65", date: new Date(2022, 7, 12) },
+    { id: "e6", title: "Lunch", amount: "$4", date: new Date(2020, 9, 8) },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
       <div>
         <NavigationBar />
       </div>
       <div>
-        <NewExpense />
+        <NewExpense onAddExpense={addExpenseHandler}/>
         <Expenses items={expenses} />
       </div>
     </div>
