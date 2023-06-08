44c1e7dae9b76430f48ba873172fc1c64ae8632b
Lukas Baltrimas <lukasbaltrimas123@gmail.com>
2023-06-08 21:28:48 +0000

    Form adds objects into array and displays it

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: "300",
+    date: new Date(2023, 1, 23),
+  },
+  {
+    id: "e2",
+    title: "Toilet paper",
+    amount: "3",
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "Car rapair",
+    amount: "689",
+    date: new Date(2021, 3, 2),
+  },
+];
+
 const App = (props) => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: "$300",
-      date: new Date(2023, 1, 23),
-    },
-    {
-      id: "e2",
-      title: "Toilet paper",
-      amount: "$3",
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "Car rapair",
-      amount: "$689",
-      date: new Date(2021, 3, 2),
-    }
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   };
+
   return (
     <div>
-      <NavigationBar/>
+      <NavigationBar />
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
@@ -40,4 +44,3 @@ const App = (props) => {
 };
 
 export default App;
- 
