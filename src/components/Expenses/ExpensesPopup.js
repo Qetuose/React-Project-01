904e63dafd51426e194b252aa55ae34e33d4d24c
Lukas Baltrimas <lukasbaltrimas123@gmail.com>
2023-06-29 20:40:51 +0000

    popup form

--- src/components/Expenses/ExpensesPopup.js
+++ src/components/Expenses/ExpensesPopup.js
@@ -2,16 +2,14 @@ import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesPopup.css";
 
-const ExpenesPopup = (props, {setPopup}) => {
+const ExpenesPopup = (props, { setPopup }) => {
   const [modal, setModal] = useState(true);
   const toggleModal = () => {
     setModal(!modal);
   };
 
-  if(!modal)
-    props.setPopup(false);
-  else
-    props.setPopup(true);
+  if (!modal) props.setPopup(false);
+  else props.setPopup(true);
 
   if (modal) {
     document.body.classList.add("active-modal");
@@ -24,13 +22,26 @@ const ExpenesPopup = (props, {setPopup}) => {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            {console.log(props.items.date)}
-            <input type="date" defaultValue={props.items.date.toISOString().substring(0,10)}/>
-            <input type="text"  defaultValue={props.items.title}/>
-            <input type="number" defaultValue={props.items.amount}/>
+            <div className="modal-date">
+            <label>Date : </label>
+              <input
+                type="date"
+                defaultValue={props.items.date.toISOString().substring(0, 10)}
+              />
+            </div>
+            <div className="modal-title">
+              <label>Title : </label>
+              <input type="text" defaultValue={props.items.title} />
+            </div>
+            <div className="modal-amount">
+            <label>Amount : </label>
+            <input type="number" defaultValue={props.items.amount} />
+            </div>
+
             <button className="close-modal" onClick={toggleModal}>
-              CLOSE
+              X
             </button>
+            <button className="save-modal">Save</button>
           </div>
         </div>
       )}
